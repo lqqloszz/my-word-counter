5bafff76f88afab4869f26ec6a2d9ce9ab60b244
lqqloszz <81083310+lqqloszz@users.noreply.github.com>
2026-06-27 21:39:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -31,4 +32,5 @@ def index():
     return render_template('index.html', count = count, seconds = seconds, speed = speed, text_content = text_content)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
